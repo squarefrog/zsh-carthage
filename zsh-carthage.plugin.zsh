ae969641a2abd31a0cc4e98b4b6096aacf010c63
Colin Drake <colin.f.drake@gmail.com>
2016-09-01 22:53:21 -0500

    Add tvOS, bootstrap platform aliases

--- zsh-carthage.plugin.zsh
+++ zsh-carthage.plugin.zsh
@@ -3,8 +3,13 @@ fpath+="`dirname $0`"
 
 # Setup aliases.
 alias cbs="carthage bootstrap $@"
+alias cbsi="carthage bootstrap --platform iOS $@"
+alias cbsw="carthage bootstrap --platform watchOS $@"
+alias cbsm="carthage bootstrap --platform Mac $@"
+alias cbst="carthage bootstrap --platform tvOS $@"
 alias cco="carthage checkout $@"
 alias cb="carthage build $@"
 alias cbi="carthage build --platform iOS $@"
 alias cbw="carthage build --platform watchOS $@"
 alias cbm="carthage build --platform Mac $@"
+alias cbt="carthage build --pltaform tvOS $@"
